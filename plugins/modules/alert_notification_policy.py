9599ef1bb9e520c3213e8c46fe779394712e39f4
Ishan Jain <ishan.jain0099@gmail.com>
2022-08-11 15:47:51 +0530

    update terms

--- plugins/modules/alert_notification_policy.py
+++ plugins/modules/alert_notification_policy.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright: (c) 2021, Rainer Leber <rainerleber@gmail.com> <rainer.leber@sva.de>
+# Copyright: (c) 2021, Ishan Jain (@ishanjainn)
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import (absolute_import, division, print_function)
