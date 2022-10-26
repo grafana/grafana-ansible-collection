e1025e2626b2987b490da0a6f84b7d2b13d5c816
ishanjainn <ishan.jain@gmail.com>
2022-10-26 12:38:33 +0530

    fixes

--- plugins/modules/cloud_stack.py
+++ plugins/modules/cloud_stack.py
@@ -147,7 +147,7 @@ def present_cloud_stack(module):
     }
     api_url = 'https://grafana.com/api/instances'
 
-    result = requests.post(api_url, json=body, headers={"Authorization": 'Bearer ' + module.params['cloud_api_key']})  
+    result = requests.post(api_url, json=body, headers={"Authorization": 'Bearer ' + module.params['cloud_api_key']})
     if result.status_code == 200:
         return False, True, result.json()
     elif result.status_code in [409, 403] and result.json()['message'] in ["That url is not available", "Hosted instance limit reached"]:
@@ -157,6 +157,7 @@ def present_cloud_stack(module):
 
             result = requests.get(api_url, headers={"Authorization": 'Bearer ' + module.params['cloud_api_key']})
             stackInfo = {}
+
             for stack in result.json()['items']:
                 if stack['slug'] == module.params['stack_slug']:
                     stack_found = True
