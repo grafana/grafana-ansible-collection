62ba6a16b0a44ddcbf97af481b51d0d14ffdab2f
ishanjainn <ishan.jain@gmail.com>
2022-10-31 12:42:11 +0530

    Sanity check fixes

--- plugins/modules/cloud_plugin.py
+++ plugins/modules/cloud_plugin.py
@@ -117,17 +117,17 @@ def present_cloud_plugin(module):
     elif result.status_code == 409:
         api_url = 'https://grafana.com/api/instances/' + module.params['stack_slug'] + '/plugins/' + module.params['name']
         result = requests.get(api_url, json={'version': module.params['version']},
-                               headers={"Authorization": 'Bearer ' + module.params['cloud_api_key']})
-        
-        if result.json()['pluginSlug'] ==  module.params['name'] and result.json()['pluginSlug'] ==  module.params['version']:
-          return False, False, result.json()             
-        else:
-          api_url = 'https://grafana.com/api/instances/' + module.params['stack_slug'] + '/plugins/' + module.params[
-              'name']
-          result = requests.post(api_url, json={'version': module.params['version']},
-                                headers={"Authorization": 'Bearer ' + module.params['cloud_api_key']})
+                              headers={"Authorization": 'Bearer ' + module.params['cloud_api_key']})
 
-          return False, True, result.json()
+        if result.json()['pluginSlug'] == module.params['name'] and result.json()['pluginSlug'] == module.params['version']:
+            return False, False, result.json()
+        else:
+            api_url = 'https://grafana.com/api/instances/' + module.params['stack_slug'] + '/plugins/' + module.params[
+                'name']
+            result = requests.post(api_url, json={'version': module.params['version']},
+                                   headers={"Authorization": 'Bearer ' + module.params['cloud_api_key']})
+
+            return False, True, result.json()
     else:
         return True, False, {"status": result.status_code, 'response': result.json()['message']}
 
