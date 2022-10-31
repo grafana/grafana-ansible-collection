7e9e6d717125df9f1cc8f11b096bcdd13b1e58b5
ishanjainn <ishan.jain@gmail.com>
2022-10-31 12:36:28 +0530

    Fixing cloud_plugin idempotency

--- plugins/modules/cloud_plugin.py
+++ plugins/modules/cloud_plugin.py
@@ -115,12 +115,19 @@ def present_cloud_plugin(module):
     if result.status_code == 200:
         return False, True, result.json()
     elif result.status_code == 409:
-        api_url = 'https://grafana.com/api/instances/' + module.params['stack_slug'] + '/plugins/' + module.params[
-            'name']
-        result = requests.post(api_url, json={'version': module.params['version']},
+        api_url = 'https://grafana.com/api/instances/' + module.params['stack_slug'] + '/plugins/' + module.params['name']
+        result = requests.get(api_url, json={'version': module.params['version']},
                                headers={"Authorization": 'Bearer ' + module.params['cloud_api_key']})
+        
+        if result.json()['pluginSlug'] ==  module.params['name'] and result.json()['pluginSlug'] ==  module.params['version']:
+          return False, False, result.json()             
+        else:
+          api_url = 'https://grafana.com/api/instances/' + module.params['stack_slug'] + '/plugins/' + module.params[
+              'name']
+          result = requests.post(api_url, json={'version': module.params['version']},
+                                headers={"Authorization": 'Bearer ' + module.params['cloud_api_key']})
 
-        return False, True, result.json()
+          return False, True, result.json()
     else:
         return True, False, {"status": result.status_code, 'response': result.json()['message']}
 
