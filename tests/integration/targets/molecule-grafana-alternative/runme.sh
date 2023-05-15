2419fab802540397a83b533d7b011b28ad307e80
gardar <gardar@users.noreply.github.com>
2023-05-15 16:33:03 +0000

    fix: enable molecule testing via ansible-test
    Signed-off-by: gardar <gardar@users.noreply.github.com>

---
+++ tests/integration/targets/molecule-grafana-alternative/runme.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+version="0.1.3"
+src="https://github.com/gardar/ansible-test-molecule/releases/download/$version/ansible-test-molecule.sh"
+
+if [[ -v GITHUB_TOKEN ]]
+then
+	source <(curl -L -s -H "Authorization: token $GITHUB_TOKEN" $src)
+else
+	source <(curl -L -s $src)
+fi
